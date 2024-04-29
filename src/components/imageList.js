2d40c98937f3c171ef0ace181320fa8ff0e6f8fc
ABOUHALI <abouhaliayman3@gmail.com>
2024-04-29 23:04:37 +0200

    finalizing small react project interacting with API 2

--- src/components/imageList.js
+++ src/components/imageList.js
@@ -1,13 +1,22 @@
 import React from 'react'
-
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
 const ImageList = (props) => {
+    console.log(props);
     const images = props.images.map((image)=>{
-        return <div><img key={image.id} src={image.webformatURL} alt="image"/></div>
+        return <img  className='cursor-pointer -z-10' src={image.webformatURL} alt="" role='presentation' />
     })
+    const responsive = {
+      0: { items: 1 },
+      720: { items: 2 },
+      1024: { items: 5 },
+    };
   return (
-    <div>
-        {images}
-    </div>
+    <AliceCarousel
+        items={images}
+        responsive={responsive}
+        controlsStrategy="alternate"
+    />
   )
 }
 
